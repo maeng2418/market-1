2a01e06abe0f76ab33eb8b9f62620836d5b7c652
MyeongSeong <maeng2418@naver.com>
2020-07-07 13:40:13 +0900

    [Add] Name 입력폼 구현 rebase #4
    Name 입력폼을 구현하고 유효성검사를 추가하였다.
    regex.js를 validate.js 으로 이름 변경하였다.

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -4,6 +4,9 @@ const idRegex = /^[a-z0-9_-]{4,20}$/;
 //비밀번호는 영문+숫자 조합만 허용하며, 8~20자.
 const pwRegex = /^[a-zA-Z0-9]{8,20}$/;
 
+//이름은 특수문자, 숫자 사용불가.
+const nameRegex = /^[가-힣a-zA-Z]+$/;
+
 const checkId = function(){
     const id = document.querySelector('.accountId');
     const idMsg = document.querySelector('.idMsg');
@@ -31,4 +34,13 @@ const confirmPw = function(){
     if(pw.value !== nextPw.value){
         confirmPwMsg.innerHTML = "위 비밀번호와 일치하지 않습니다. 다시 입력해 주세요."
     }
+}
+
+const checkName = function(){
+    const name = document.querySelector('.name');
+    const nameMsg = document.querySelector('.nameMsg');
+    if(!nameRegex.test(name.value)){
+        nameMsg.innerHTML = "이름에 특수문자, 숫자를 사용할 수 없습니다."
+    }
+
 }
