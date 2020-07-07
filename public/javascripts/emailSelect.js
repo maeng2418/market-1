2d2c27ca50a795c11f8d81d917c17a08e54e470f
MyeongSeong <maeng2418@naver.com>
2020-07-07 11:03:04 +0900

    [Add] Email 입력폼 구현
    Email 입력폼을 구현하였고, Select를 이용해 직접입력 및 주소 선택기능을 추가하였다.

---
+++ public/javascripts/emailSelect.js
@@ -0,0 +1,11 @@
+const onEmailHandler = function () {
+    const emailSelector = document.querySelector('.emailSelect');
+    const emailAddr = document.querySelector('.emailAddr');
+    if (emailSelector.value === "직접입력"){
+        emailAddr.value = "";
+        emailAddr.readOnly = false;
+    }else{
+        emailAddr.value = emailSelector.value;
+        emailAddr.readOnly = true;
+    }
+}
