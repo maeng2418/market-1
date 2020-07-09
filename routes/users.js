7a76883c250e47a522a7406c294f9b7fc64664fe
MyeongSeong <maeng2418@naver.com>
2020-07-09 17:25:02 +0900

    [Add] 로그인시 JWT 발행 및 쿠키 생성
    로그인시 jsonwebtoken으로 JWT를 쿠키로 생성

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,14 @@ const Database = require('nedb');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const moment = require("moment");
+
+function toHexString(byteArray) {
+  return Array.from(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('')
+}
 
 const db = new Database({filename: path.join(__dirname, '../database/account.db'), autoload:true});
 
@@ -42,7 +50,12 @@ router.post('/signin', function(req, res, next) {
       bcrypt.compare(req.body.pwd, docs[0].password, (err, isMatch)=>{
         if(err) return res.status(400).send(err);
         if(isMatch){
-          return res.status(200).json({success: true, name : docs[0].name})
+          // jwt 토큰 생성
+          const token = jwt.sign(toHexString(docs[0]._id), 'secretToken'); // _id와 'secretToken' 을 합쳐서 토큰을 생성
+          const oneHour = moment().add(1, 'hour').valueOf();
+          db.update({ id: req.body.id }, { $set: { "token": token } }, {}, function () {
+            return res.cookie("jwt", token, { expires: new Date(oneHour), httpOnly: true }).status(200).json({ success: true, name : docs[0].name });
+          });
         }else{
           return res.status(400).json({success: false})
         }
