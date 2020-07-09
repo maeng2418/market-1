d891455febd01687b1f894271ee249b8d09b4ec2
woowahan17 <woowahan17@MacBook-Pro.local>
2020-07-09 14:18:34 +0900

    임시 중간 커밋

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//회원가입
 router.post('/signup', function(req, res, next) {
   console.log(req.body);
   db.insert(req.body, (err, userInfo) =>{
@@ -17,5 +18,20 @@ router.post('/signup', function(req, res, next) {
   })
   return res.status(200).json({success: true})
 });
+
+//로그인
+router.post('/signin', function(req, res, next) {
+  console.log("로그인 정보 :", req.body);
+  const inputId = req.body.id;
+  db.find({ id: inputId }, function(err, docs){
+    console.log(docs);
+    if(docs.length !== 0){
+      
+    }else{
+
+    }
+  })
+});
+
 
 module.exports = router;
