fb1499fab1adde1453bce2467083c0702f0e783c
MyeongSeong <maeng2418@naver.com>
2020-07-09 17:43:55 +0900

    [Add] auth 미들웨어 구현
    쿠키를 조회해서 유저 정보를 넘겨주는 미들웨어를 구현하였습니다.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,28 @@
-// let auth = (req, res, next) => {
+const Database = require('nedb');
+const db = new Database({ filename: path.join(__dirname, '../database/account.db')});
 
-//     // 인증 처리를 하는 곳
+let auth = (req, res, next) => {
 
-//     // 토큰을 가져온다.
-//     let token = req.cookies.jwt;
+    // 인증 처리를 하는 곳
+
+    // 토큰을 가져온다.
+    let token = req.cookies.jwt;
     
-//     // 토큰을 복호화한 후 유저를 찾는다.
-//     User.findByToken(token, (err, user) => {
-//         if(err) throw err;
-//         if(!user) return res.json({ isAuth: false, error: true })
+    // 토큰을 복호화한 후 유저를 찾는다.
+    db.loadDatabase(function(err){
+        db.find({token}, function(err, user){
+            if(err) throw err;
+            if(!user[0]) return res.json({ isAuth: false, error: true })
+    
+            req.token = token; // 토큰과 유저 가져 올 수 있음
+            req.user = user[0];
+            next(); // 다음으로...
+        });
+    });
 
-//         req.token = token; // 토큰과 유저 가져 올 수 있음
-//         req.user = user;
-//         next(); // 다음으로...
-//     });
+    // 유저가 있으면 인증 OKay
 
-//     // 유저가 있으면 인증 OKay
+    // 유저가 없으면 인증 No !
+}
 
-//     // 유저가 없으면 인증 No !
-// }
-
-// module.exports = { auth };
+module.exports = { auth };
